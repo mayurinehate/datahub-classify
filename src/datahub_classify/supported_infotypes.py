1fef07e75950976559e11882bd65f05a4bbe3be9
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-10-19 17:40:46 +0530

    added 6 new infotypes

--- src/datahub_classify/supported_infotypes.py
+++ src/datahub_classify/supported_infotypes.py
@@ -1,2 +1,3 @@
-infotypes_to_use = ['Street_Address', 'Gender', 'Credit_Debit_Card_Number', 'Email_Address', 'Phone_Number', 'Full_Name',
-                    'Age']
+infotypes_to_use = ['Street_Address', 'Gender', 'Credit_Debit_Card_Number', 'Email_Address',
+                    'Phone_Number', 'Full_Name','Age', 'IBAN', 'Vehicle_Identification_Number',
+                    'US_Social_Security_Number','IP_Address_v4','IP_Address_v6','Swift_Code']
