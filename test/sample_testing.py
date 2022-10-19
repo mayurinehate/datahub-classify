47fbe516ac0f39f6f444c51246cb7da84a2201fb
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-10-19 17:44:27 +0530

    added new datasets for testing

--- test/sample_testing.py
+++ test/sample_testing.py
@@ -24,6 +24,7 @@ current_wdr = os.getcwd()
 input_data_dir = current_wdr + "\\datasets\\"
 input_jsons_dir = current_wdr + "\\expected_output\\"
 confidence_threshold = 0.6
+update_confidence_slabs_json = False
 
 logging_directory = current_wdr + "/logs/logs.log"
 
@@ -38,7 +39,7 @@ def get_public_data(input_data_path):
     data6 = pd.read_csv(input_data_path + "catalog.csv")
     data7 = pd.read_csv(input_data_path + "iban.csv")
     data12 = pd.read_csv(input_data_path + "2018-seattle-business-districts.csv")
-    data13 = pd.read_csv(input_data_path + "Customer Segmentation.csv")
+    data13 = pd.read_csv(input_data_path + "Customer_Segmentation.csv")
     data14 = pd.read_csv(input_data_path + "application_record.csv")
     data15 = pd.read_csv(input_data_path + "Airbnb_Open_Data.csv")
     data16 = pd.read_csv(input_data_path + "Book1.xlsx-credit-card-number.csv")
@@ -49,11 +50,40 @@ def get_public_data(input_data_path):
     data28 = pd.read_csv(input_data_path + "CrabAgePrediction.csv")
     data29 = pd.read_csv(input_data_path + "Salary_Data.csv")
     data30 = pd.read_csv(input_data_path + "drug-use-by-age.csv")
+    data31 = pd.read_csv(input_data_path + "Book1.xlsx-us-social-security-22-cvs.csv")
+    data32 = pd.read_csv(input_data_path + "sample-data.csv")
+    data33 = pd.read_excel(input_data_path + "1-MB-Test.xlsx")
+    data34 = pd.read_csv(input_data_path + "random_ibans.csv")
+    data35 = pd.read_csv(input_data_path + "used_cars_data.csv", nrows = 1000)
+    data36 = pd.read_csv(input_data_path + "train.csv", nrows = 1000)
+    data37 = pd.read_csv(input_data_path + "test.csv", nrows = 1000)
+    data38 = pd.read_csv(input_data_path + "vehicles_1.csv", nrows = 1000)
+    data39 = pd.read_csv(input_data_path + "vehicles_2.csv", nrows = 1000)
+    data40 = pd.read_csv(input_data_path + "vehicles_3.csv", nrows = 1000)
+    data41 = pd.read_csv(input_data_path + "Dataset-Unicauca-Version2-87Atts_1.csv")
+    data42 = pd.read_csv(input_data_path + "Dataset-Unicauca-Version2-87Atts_2.csv")
+    data43 = pd.read_csv(input_data_path + "Dataset-Unicauca-Version2-87Atts_3.csv")
+    data44 = pd.read_csv(input_data_path + "Dataset-Unicauca-Version2-87Atts_4.csv")
+    data45 = pd.read_csv(input_data_path + "Dataset-Unicauca-Version2-87Atts_5.csv")
+    data46 = pd.read_csv(input_data_path + "visitor-interests.csv", nrows = 1000)
+    data47 = pd.read_csv(input_data_path + "Darknet_.csv", nrows = 1000,on_bad_lines='skip')
+    data48 = pd.read_csv(input_data_path + "vehicles_4.csv")
+    data49 = pd.read_csv(input_data_path + "vehicles_5.csv")
+    data50 = pd.read_csv(input_data_path + "Device Report - BU175-VPC2021-03-21_11-00-03.csv")
+    data51 = pd.read_csv(input_data_path + "2021-04-23_honeypot-cloud-digitalocean-geo-1_netflow-extended.csv", nrows=1000)
+    data52 = pd.read_csv(input_data_path + "ipv6_random_generated.csv")
+    data53 = pd.read_csv(input_data_path + "score-banks-updated-sep2022.csv")
+    data54 = pd.read_csv(input_data_path + "blz-aktuell-xlsx-data.csv")
     return {'data1': data1, 'data2': data2, 'data3': data3, 'data4': data4, 'data5': data5,
             'data6': data6, 'data7': data7, 'data12': data12, 'data13': data13, 'data14': data14,
             'data15': data15,'data16': data16, 'data17': data17, 'data21': data21, 'data25': data25,
-            'data27': data27, 'data28': data28, 'data29': data29, 'data30': data30}
-
+            'data27': data27, 'data28': data28, 'data29': data29, 'data30': data30, 'data31': data31,
+            'data32': data32, 'data33': data33, 'data34': data34, 'data35': data35,
+            'data36': data36, 'data37': data37, 'data38': data38, 'data39': data39,
+            'data40': data40, 'data41': data41, 'data42': data42, 'data43': data43,
+            'data44': data44, 'data45': data45, 'data46': data46, 'data47': data47,
+            'data48': data48, 'data49': data49, 'data50': data50, 'data51': data51,
+            'data52': data52, 'data53': data53, 'data54': data54 }
 
 def populate_column_info_list(public_data_list):
     column_info_list = []
@@ -101,15 +131,18 @@ def get_public_data_expected_output(public_data_list, infotypes_to_use):
     return expected_output_ideal, expected_output_unit_testing, expected_infotypes_confidence_slabs
 
 
-def get_best_infotype_pred(public_data_list, confidence_threshold, expected_output_unit_testing):
+def get_best_infotype_pred(public_data_list, confidence_threshold, expected_output_unit_testing,
+                           update_confidence_slabs_json= False):
+    with open(input_jsons_dir + "expected_infotypes_confidence_slabs.json") as filename:
+        old_confidence_slabs = json.load(filename)
     column_info_list = populate_column_info_list(public_data_list)
     column_info_pred_list = predict_infotypes(column_info_list, confidence_threshold, input_dict)
     public_data_predicted_infotype = dict()
-    # get_thresholds_for_unit_test = dict()
     public_data_predicted_infotype_confidence = dict()
     for dataset in public_data_list.keys():
         public_data_predicted_infotype[dataset] = dict()
-        # get_thresholds_for_unit_test[dataset] = dict()
+        if not old_confidence_slabs.get(dataset):
+            old_confidence_slabs[dataset] = dict()
         public_data_predicted_infotype_confidence[dataset] = dict()
         for col in public_data_list[dataset].columns:
             for col_info in column_info_pred_list:
@@ -120,9 +153,10 @@ def get_best_infotype_pred(public_data_list, confidence_threshold, expected_outp
                         infotype_assigned = None
                         for i in range(len(col_info.infotype_proposals)):
                             if col_info.infotype_proposals[i].confidence_level > highest_confidence_level:
-                                # get_thresholds_for_unit_test[dataset][col] = col_info.infotype_proposals[i].confidence_level
                                 highest_confidence_level = col_info.infotype_proposals[i].confidence_level
                                 infotype_assigned = col_info.infotype_proposals[i].infotype
+                        if not old_confidence_slabs[dataset].get(col):
+                            old_confidence_slabs[dataset][col] = np.floor(highest_confidence_level * 10) / 10
                         public_data_predicted_infotype[dataset][col] = infotype_assigned
                         public_data_predicted_infotype_confidence[dataset][col] = highest_confidence_level
 
@@ -134,8 +168,9 @@ def get_best_infotype_pred(public_data_list, confidence_threshold, expected_outp
                         if expected_output_unit_testing[dataset][col] not in (infotypes_to_use + ["no_infotype"]):
                             expected_output_unit_testing[dataset][col] = "no_infotype"
                         public_data_predicted_infotype_confidence[dataset][col] = 0.0
-    # with open("infotype_threholds.json", "w") as filename:
-    #     json.dump(get_thresholds_for_unit_test, filename)
+    if update_confidence_slabs_json:
+        with open(input_jsons_dir + "expected_infotypes_confidence_slabs.json", "w") as filename:
+            json.dump(old_confidence_slabs, filename)
     return public_data_predicted_infotype, expected_output_unit_testing, public_data_predicted_infotype_confidence
 
 
@@ -185,7 +220,8 @@ expected_output_ideal, expected_output_unit_testing, expected_infotypes_confiden
 public_data_predicted_infotype, expected_output_unit_testing, public_data_predicted_infotype_confidence =\
                                                                                       get_best_infotype_pred(public_data_list,
                                                                                       confidence_threshold,
-                                                                                      expected_output_unit_testing)
+                                                                                      expected_output_unit_testing,
+                                                                                        update_confidence_slabs_json)
 infotype_mapping_ideal = get_pred_exp_infotype_mapping(public_data_predicted_infotype,
                                                        expected_output_ideal,
                                                        expected_infotypes_confidence_slabs,
