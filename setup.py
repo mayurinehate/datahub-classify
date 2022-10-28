949cbca1e414d462c2370073d2968f1cacc2760a
Krushna <krushnakant.mardikar@gslab.com>
2022-10-28 21:24:44 +0530

    Mentioned the required libraries

--- setup.py
+++ setup.py
@@ -15,10 +15,15 @@ def get_long_description():
 base_requirements = {
     "spacy==3.4.1",
     "phonenumbers==8.12.56",
+    "vininfo==1.7.0",
+    "schwifty==2022.9.0",
+    "python-stdnum==1.17",
+    "dlnvalidation",
     "pandas==1.5.0",
     "scikit-learn==1.1.2",
     "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.4.0/en_core_web_sm-3.4.0.tar.gz",
-    "pytest"
+    "pytest",
+    "openpyxl"
 }
 
 setuptools.setup(
